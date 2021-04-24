3b0127cf08d99a65231dc3b371af43f579d16002
Salvatore <salvadduci@gmail.com>
2021-04-24 17:20:28 +0200

    Jump
    Modifiche al movimento e al salto

--- Assets/Scripts/Player/FPSInput.cs
+++ Assets/Scripts/Player/FPSInput.cs
@@ -5,33 +5,45 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]   // fa comparire nel menu del character controller lo script
 [AddComponentMenu("Control Script/FPS Input")]
-public class FPSInput : MonoBehaviour
-{
-
+public class FPSInput : MonoBehaviour {
     public float speed = 6.0f;
     public float gravity = -9.8f;
-    private CharacterController _charController;
+    public float jumpHeight = 3f;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        _charController = GetComponent<CharacterController>();        
-        
+    public Transform groundCheck;
+    public float gorundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 movement;
+    bool isGrounded;
+
+    private CharacterController _charController;
+
+    void Start() {
+        _charController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
-        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed);        
-        
-        movement.y = gravity;
+        isGrounded = Physics.CheckSphere(groundCheck.position, gorundDistance, groundMask);
 
-        movement *= Time.deltaTime;
-        movement = transform.TransformDirection(movement);
-        _charController.Move(movement);
+         if(isGrounded && movement.y < 0){
+            movement.y = -2f;
+        }
+
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaZ = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * deltaX + transform.forward * deltaZ;
+	    //movement = Vector3.ClampMagnitude(movement, speed);
+
+        _charController.Move(move * speed * Time.deltaTime);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+            movement.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        movement.y += gravity * Time.deltaTime;
+        _charController.Move(movement * Time.deltaTime);
+
     }
 }
