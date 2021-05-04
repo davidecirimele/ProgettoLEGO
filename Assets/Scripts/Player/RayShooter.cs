137f16e5a453c486ee065eb465f938f6cb4e9f7e
Salvatore <salvadduci@gmail.com>
2021-05-04 14:52:07 +0200

    Camera
    Camera funzionante

--- Assets/Scripts/Player/RayShooter.cs
+++ Assets/Scripts/Player/RayShooter.cs
@@ -8,7 +8,8 @@ public class RayShooter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; // lock mouse on the center 
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
--- Assets/Scripts/Player/TPSInput.cs
+++ Assets/Scripts/Player/TPSInput.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class TPSInput : MonoBehaviour
+{
+    [SerializeField] private CharacterController _charController;
+    [SerializeField] public Transform cam;
 
-[RequireComponent(typeof(CharacterController))]   // fa comparire nel menu del character controller lo script
-[AddComponentMenu("Control Script/TPS Input")]
-public class FPSInput : MonoBehaviour {
+
     public float speed = 6.0f;
     public float gravity = -9.8f;
     public float jumpHeight = 3f;
@@ -17,7 +19,9 @@ public class FPSInput : MonoBehaviour {
     Vector3 movement;
     bool isGrounded;
 
-    private CharacterController _charController;
+    
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     void Start() {
         _charController = GetComponent<CharacterController>();
@@ -37,7 +41,13 @@ public class FPSInput : MonoBehaviour {
         Vector3 direction = new Vector3(deltaX, 0f, deltaZ).normalized;
 
         if(direction.magnitude >= 0.1f){
-            _charController.Move(direction * speed * Time.deltaTime);
+            
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            _charController.Move(moveDir.normalized * speed * Time.deltaTime);
         }
         //Vector3 move = transform.right * deltaX + transform.forward * deltaZ;
 	    //movement = Vector3.ClampMagnitude(movement, speed);
