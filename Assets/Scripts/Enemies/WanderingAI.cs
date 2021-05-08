33fe520452e641789111957b6215b20daba6b63d
giusep1995 <giusep1995@gmail.com>
2021-05-08 17:48:50 +0200

    WanderingAI now is working

--- Assets/Scripts/Enemies/WanderingAI.cs
+++ Assets/Scripts/Enemies/WanderingAI.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [AddComponentMenu("Control Script/AlienScript")]
 public class WanderingAI : MonoBehaviour
 {
+    [SerializeField]
+    float speed2 = 20;
+
+    [SerializeField] List<Transform> wayPoint;
+    public GameObject WaypointParent;
+    [SerializeField] private int waypointPosition;
+    NavMeshAgent navMeshAgent;
+    public int range = 1;
+
     [SerializeField] private GameObject laserPrefab;
     private GameObject _laser;
 
@@ -13,18 +23,23 @@ public class WanderingAI : MonoBehaviour
 
     private bool _alive;
 
-    // Start is called before the first frame update
     void Start()
     {
-      _alive = true;   
+        _alive = true;
+        //initialise the target way point
+        for (int i = 0; i < WaypointParent.transform.childCount; i++)
+            wayPoint.Add(WaypointParent.transform.GetChild(i));
+        waypointPosition = 0;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(wayPoint[waypointPosition].position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(_alive){
-            //transform.Translate(0, 0, speed * Time.deltaTime);
-
+        if(_alive){  
+            if (verifyInRange(range, wayPoint[waypointPosition].position))
+                Move();
+       
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
             if(Physics.SphereCast(ray, 0.75f, out hit)){
@@ -57,5 +72,22 @@ public class WanderingAI : MonoBehaviour
 
     public void setAlive(bool alive){
         _alive = alive;
+    }
+
+    bool verifyInRange(int range, Vector3 pos)
+    {
+        if (transform.position.x < pos.x + 1 && transform.position.x > pos.x - 1)
+            if (transform.position.z < pos.z + 1 && transform.position.z > pos.z - 1)
+            {
+                return true;
+            }
+        return false;
+    }
+
+    void Move()
+    {
+        navMeshAgent.SetDestination(wayPoint[++waypointPosition].position);
+        print(wayPoint);
+        if (waypointPosition == wayPoint.Count) waypointPosition = 0;
     }
 }
