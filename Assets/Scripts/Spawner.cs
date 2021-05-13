8378f14e28f9ad4c76bb25fec56886328a3c3748
Salvatore <salvadduci@gmail.com>
2021-05-13 13:21:22 +0200

    spawn oggetti
    prototipo spawn oggetti

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+   [SerializeField] private GameObject spawnee;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1)){
+           
+           Vector3 mouseScreenPosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
+            if(Physics.Raycast(ray, out RaycastHit hitInfo)){
+               SpawnObjectAtPositon(hitInfo.point);
+            }
+        }
+    }
+
+    private void SpawnObjectAtPositon(Vector3 spawnPosition){
+        GameObject obj = Instantiate(spawnee, spawnPosition, Quaternion.identity);
+    }
+}
