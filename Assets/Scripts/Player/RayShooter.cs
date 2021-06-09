18c280eab76d2c1a2f6e76b3eed5cb89b5247ef5
giusep1995 <giusep1995@gmail.com>
2021-06-09 18:33:25 +0200

    Now the player have ik

--- Assets/Scripts/Player/RayShooter.cs
+++ Assets/Scripts/Player/RayShooter.cs
@@ -19,9 +19,9 @@ public class RayShooter : MonoBehaviour
     {
         time = 0.05f;
         layerMask = ~ (1 << LayerMask.NameToLayer("Player"));
-        target = Camera.main.transform; // The Camera utilized by the character
+        //target = Camera.main.transform; // The Camera utilized by the character
         Cursor.lockState = CursorLockMode.Locked; // lock mouse on the center 
-        Cursor.visible = true;
+        //Cursor.visible = true;
 
         _soundSource = GetComponent<AudioSource>();
     }
@@ -29,9 +29,10 @@ public class RayShooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()){
+        Debug.DrawRay(target.position, (transform.position - target.position) * 100, Color.red, 0.1f);
+            if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
             _soundSource.PlayOneShot(shotSound);
-            Ray ray = new Ray(target.position, (transform.position - target.position) * 10);
+            Ray ray = new Ray(target.position, (transform.position - target.position) * 100);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 100f, layerMask)){
                 SpawnBulletTrail(hit.point);
@@ -40,16 +41,16 @@ public class RayShooter : MonoBehaviour
                 if (hit.transform.tag == "Destroyable")
                     hit.transform.GetComponent<DestroyAndDrop>().Damage();
                 if (hit.transform.tag == "Boss")
-                    if(hit.transform.GetComponent<BossLife>()!=null)
+                    if (hit.transform.GetComponent<BossLife>() != null)
                         hit.transform.GetComponent<BossLife>().Hitted(1);
-                        else{
-                            BossLife = GameObject.Find("TankFree_Yel");
-                            BossLife.GetComponent<BossLife>().Hitted(1);
-                        }
+                    else
+                    {
+                        BossLife = GameObject.Find("TankFree_Yel");
+                        BossLife.GetComponent<BossLife>().Hitted(1);
+                    }
                 if (hit.transform.tag == "BossAlien")
                     hit.transform.GetComponent<ReactiveBoss>().ReactToHit();
                 StartCoroutine(SpawnBulletTrail(hit.point));
-               
                 if (target != null)
                     target.ReactToHit(); //this function is in target Script
             }
@@ -65,7 +66,7 @@ public class RayShooter : MonoBehaviour
         laser.GetComponent<BoxCollider>().enabled = false;
         laser.transform.localScale = new Vector3(0.05f, 0.05f, 0.4f);
         laser.transform.position = transform.position;
-        laser.transform.rotation = Quaternion.FromToRotation(hitPoint, transform.position - hitPoint);
+        laser.transform.rotation = Quaternion.LookRotation(hitPoint - transform.position, Vector3.up);
         float i = 0f;
         float rate = 1.0f / time;
         while (i < 1.0f)
