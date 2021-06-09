6540c15b6935939fa1e20fc33d7ee2aeb8847241
Salvatore <salvadduci@gmail.com>
2021-06-09 19:02:20 +0200

    change
    change

--- Assets/Scripts/FallScript.cs
+++ Assets/Scripts/FallScript.cs
@@ -8,8 +8,7 @@ public class FallScript : MonoBehaviour
       PlayerCharacter player = other.GetComponent<PlayerCharacter>();
        
       if(player != null){
-         player.Hurt(1);
-         player.transform.position += new Vector3(0, 20, 0);
+         player.Death();
       }
    }
 }
