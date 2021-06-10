060c93ec87d718f20a0360bdccf658785f38c94e
giusep1995 <giusep1995@gmail.com>
2021-06-10 15:57:38 +0200

    Now we have a "creation" mode

--- Assets/Scripts/World/SpawnerManager.cs
+++ Assets/Scripts/World/SpawnerManager.cs
@@ -6,28 +6,54 @@ using UnityEngine.EventSystems;
 public class SpawnerManager : MonoBehaviour, IGameManager
 {
    [SerializeField] private List<GameObject> spawnableObjects;
-   [SerializeField] private GameObject spawnee;
-   [SerializeField] private Transform player;
+    [SerializeField] private List<GameObject> spawnableObjectsMockup;
+    [SerializeField] private GameObject spawnee;
+    [SerializeField] private GameObject spawneeMockup;
+    [SerializeField] private GameObject objectMesh;
+    [SerializeField] private Transform player;
 
+    private bool isPlacing = false;
     public ManagerStatus status { get; private set; }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(1) && !EventSystem.current.IsPointerOverGameObject()){
-           
-           Vector3 mouseScreenPosition = Input.mousePosition;
-            Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
-            if(Physics.Raycast(ray, out RaycastHit hitInfo)){
-               SpawnObjectAtPositon(hitInfo.point);
+        if (!EventSystem.current.IsPointerOverGameObject()) {
+            if (Input.GetMouseButtonDown(1) && Managers.Inventory.checkForCreation(getObjectName())) {
+                isPlacing = true;
+                SpawnMockupObjectAtPositon(new Vector3(0f, 0f, 0f));
+            }
+
+            if (isPlacing) {
+                Vector3 mouseScreenPosition = Input.mousePosition;
+                Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
+                if (Physics.Raycast(ray, out RaycastHit hitInfo))
+                {
+                    objectMesh.transform.position = hitInfo.point + objectMesh.GetComponent<Offset>().getOffset();
+                    objectMesh.transform.rotation = new Quaternion(0f, player.transform.rotation.y, 0f, player.transform.rotation.w);
+                }
+            }
+
+            if (Input.GetMouseButtonUp(1)) {
+                Destroy(objectMesh);
+                Vector3 mouseScreenPosition = Input.mousePosition;
+                Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
+                if (Physics.Raycast(ray, out RaycastHit hitInfo))
+                {
+                    SpawnObjectAtPositon(hitInfo.point);
+                }
             }
         }
     }
+    
+    private void SpawnMockupObjectAtPositon(Vector3 spawnPosition)
+    {
+        objectMesh = Instantiate(spawneeMockup, spawnPosition + spawnee.GetComponent<Offset>().getOffset(), new Quaternion(0f, player.transform.rotation.y, 0f, player.transform.rotation.w));
+    }
 
     private void SpawnObjectAtPositon(Vector3 spawnPosition){
-        if(Managers.Inventory.checkForCreation(getObjectName())){
-            Managers.Audio.CreateObject();
-            GameObject obj = Instantiate(spawnee, spawnPosition + spawnee.GetComponent<Offset>().getOffset(), new Quaternion(0f, player.transform.rotation.y, 0f, player.transform.rotation.w));
-        }
+        Managers.Audio.CreateObject();
+        GameObject obj = Instantiate(spawnee, spawnPosition + spawnee.GetComponent<Offset>().getOffset(), new Quaternion(0f, player.transform.rotation.y, 0f, player.transform.rotation.w));
+        isPlacing = false;
     }
 
     public void Startup()
@@ -35,11 +61,15 @@ public class SpawnerManager : MonoBehaviour, IGameManager
         Debug.Log("Spawner Objects manager starting...");
         spawnableObjects = new List<GameObject>();
         foreach(Object obj in Resources.LoadAll("Spawnable", typeof(GameObject)))
-        {
             spawnableObjects.Add(Instantiate(obj) as GameObject);
-        }
+        
+        foreach(Object obj in Resources.LoadAll("SpawnableMockup", typeof(GameObject)))
+            spawnableObjectsMockup.Add(Instantiate(obj) as GameObject);
         if (spawnableObjects.Count != 0)
+        {
             spawnee = spawnableObjects[2];
+            spawneeMockup = spawnableObjectsMockup[2];
+        }
         status = ManagerStatus.Started;
     }
 
@@ -55,7 +85,10 @@ public class SpawnerManager : MonoBehaviour, IGameManager
 
     public void SetSpawnee(int chObj){
         if (spawnableObjects.Count != 0)
+        {
             Managers.Audio.ChangeObjectSpawn();
             spawnee = spawnableObjects[chObj];
+            spawneeMockup = spawnableObjectsMockup[chObj];
+        }
     }
 }
